31061bad50b07ab996be485a69848a994c30ee59
andri <assliusarenko@gmail.com>
2021-02-15 14:41:05 +0200

    update declarations and imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AllResultsComponent } from './components/results/all-results/all-results.component';
+import { ResultComponent } from './components/results/result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchBarComponent,
+    AllResultsComponent,
+    ResultComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
