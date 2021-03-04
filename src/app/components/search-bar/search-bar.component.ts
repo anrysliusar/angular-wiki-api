683a2e0e8d5cedc7952d1350ff21eb998fd80ffd
andri <assliusarenko@gmail.com>
2021-03-04 20:22:36 +0200

    remove dropdown, add autocomplete

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -7,10 +7,14 @@ import {FormControl, FormGroup} from '@angular/forms';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
+  private options = [];
+  suitableOptions = [];
   definition = new FormControl();
+
   contactForm = new FormGroup({
     name: this.definition
   });
+
   @Output()
   definitionEmitter = new EventEmitter();
 
@@ -21,7 +25,40 @@ export class SearchBarComponent implements OnInit {
   }
 
   contactFormSubmit(): void {
-    this.definitionEmitter.emit(this.contactForm.value.name);
+    if (this.contactForm.value.name === null) {
+      alert('Wrong input!');
+      return;
+    }
+
+    this.options = this.getDataFromLocalStorage();
+    const formTerm = this.contactForm.value.name;
+    this.definitionEmitter.emit(formTerm);
+
+    if (!localStorage.length) {
+      localStorage.setItem(`term${localStorage.length}`, formTerm);
+      return;
+    }
+
+    if (!this.options.includes(formTerm)) {
+      localStorage.setItem(`term${localStorage.length}`, formTerm);
+    }
   }
 
+  onChangeFormComplete(): void {
+    this.suitableOptions = [];
+    this.options = this.getDataFromLocalStorage();
+    for (const option of this.options) {
+      if (option.toLowerCase().includes(this.contactForm.value.name.toLowerCase())) {
+        this.suitableOptions.push(option);
+      }
+    }
+  }
+
+  getDataFromLocalStorage(): string[] {
+    const array = [];
+    for (let i = 0; i < localStorage.length; i++) {
+      array.push(localStorage.getItem(`term${i}`));
+    }
+    return array;
+  }
 }
