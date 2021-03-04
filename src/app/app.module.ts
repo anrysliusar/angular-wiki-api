0f47f7ff933e4cc7564058f14d5eb7b3202918f4
andri <assliusarenko@gmail.com>
2021-03-04 20:23:01 +0200

    update imported modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,32 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AllResultsComponent } from './components/results/all-results/all-results.component';
 import { ResultComponent } from './components/results/result/result.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBarComponent,
     AllResultsComponent,
-    ResultComponent
+    ResultComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatSelectModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
