ba5cfe0a3b0866c738552cba6906017989cc105f
andri <assliusarenko@gmail.com>
2021-03-05 11:01:38 +0200

    added searcher (nav item)

---
+++ src/app/components/searcher/searcher/searcher.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {WikiService} from '../../../services/wiki.service';
+
+@Component({
+  selector: 'app-searcher',
+  templateUrl: './searcher.component.html',
+  styleUrls: ['./searcher.component.css']
+})
+export class SearcherComponent implements OnInit {
+  results = [];
+  constructor(private wikiService: WikiService) { }
+
+  ngOnInit(): void {
+  }
+
+  getWikiDefinition(definition: string): void {
+    this.wikiService
+      .search(definition)
+      .subscribe(value => {
+        this.results = value.query.search;
+        console.log(this.results);
+      });
+  }
+}
